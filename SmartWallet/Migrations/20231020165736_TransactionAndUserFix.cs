534b165c48b7fd7a68ac4ddcd87c5da425d759e3
Yura <sokolovskiiyura@gmail.com>
2023-10-20 20:00:51 +0300

    migrations added

---
+++ SmartWallet/Migrations/20231020165736_TransactionAndUserFix.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartWallet.Migrations
+{
+    public partial class TransactionAndUserFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cards_Users_UserId",
+                table: "Cards");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Users_UserId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "SenderCardId",
+                table: "Transactions",
+                newName: "OperationType");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RecipientId",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RecipientCardId",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Amount",
+                table: "Transactions",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Cards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_RecipientCardId",
+                table: "Transactions",
+                column: "RecipientCardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_RecipientId",
+                table: "Transactions",
+                column: "RecipientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cards_Users_UserId",
+                table: "Cards",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Cards_RecipientCardId",
+                table: "Transactions",
+                column: "RecipientCardId",
+                principalTable: "Cards",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Users_RecipientId",
+                table: "Transactions",
+                column: "RecipientId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cards_Users_UserId",
+                table: "Cards");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Cards_RecipientCardId",
+                table: "Transactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Users_RecipientId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_RecipientCardId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_RecipientId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "OperationType",
+                table: "Transactions",
+                newName: "SenderCardId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RecipientId",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RecipientCardId",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Transactions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Cards",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transactions",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cards_Users_UserId",
+                table: "Cards",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Users_UserId",
+                table: "Transactions",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
