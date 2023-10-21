520d46ce04338c1a46aee439ae0a356fef6bf422
YuraSokolovskyi <sokolovskiiyura@gmail.com>
2023-10-21 14:43:47 -0400

    MyCards control added

---
+++ SmartWallet/UI/Controls/CardControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using SmartWallet.DAL.Entity;
+
+namespace SmartWallet.UI.Controls;
+
+public partial class CardControl : UserControl
+{
+    public static readonly DependencyProperty CardProperty = DependencyProperty.Register(
+        "Card", typeof(Card), typeof(CardControl), new PropertyMetadata(null));
+
+    public Card CardData
+    {
+        get { return (Card)GetValue(CardProperty); }
+        set
+        {
+            SetValue(CardProperty, value);
+            Balance.Text = value.Balance.ToString("N2");
+            Number.Text = formatCardNumber(value.Number);
+            DateExpire.Text = formatDateExpire(value.DateExpire);
+        }
+    }
+    
+    public CardControl()
+    {
+        InitializeComponent();
+    }
+
+    private string formatDateExpire(DateTime dateExpire)
+    {
+        return (dateExpire.Month < 10 ? "0" + dateExpire.Month : dateExpire.Month) + "/" + dateExpire.Year.ToString().Substring(2);
+    }
+    
+    private string formatCardNumber(string number)
+    {
+        return $"{number.Substring(0, 4)} {number.Substring(4, 4)} {number.Substring(8, 4)} {number.Substring(12)}";
+    }
+}
